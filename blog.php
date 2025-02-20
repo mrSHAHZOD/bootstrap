7c65064ecf7a8076726d92a0949a347e3fcd301e
mrSHAHZOD <shahzodzokirov0304@gmail.com>
2025-02-20 15:50:24 +0500

    Post id show

--- blog.php
+++ blog.php
@@ -35,6 +35,7 @@ $posts = $statement->fetchAll();
       </div>
     <?php endif; ?>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
       <?php foreach($posts as $post) : ?>
       <div class="col">
         <div class="card shadow-sm">
@@ -43,6 +44,9 @@ $posts = $statement->fetchAll();
             <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
           </svg>
           <div class="card-body">
+            <a href="post.php?post_id=<?= $post['id'] ?>">
+              <h4><?= $post['title'] ?></h4>
+            </a>
             <h4><?= $post["title"] ?></h4>
             <p class="card-text"><?= $post['body'] ?></p>
             <div class="d-flex justify-content-between align-items-center">
--- post.php
+++ post.php
@@ -1,30 +1,24 @@
 <?php
 $title = 'Post';
 require 'includs/header.php';
+
+require 'database.php';
+
+$id = $_GET['post_id'];
+$statement = $pdo->prepare("SELECT * FROM posts where id = ?");
+$statement->execute([$id]);
+$post = $statement->fetch();
+
 ?>
 
 <div class="container mt-5" >
-<h1>Get started with Bootstrap</h1>
-    <p class="fs-5 col-md-8">Quickly and easily get started with Bootstrap's compiled, production-ready files with this barebones example featuring some basic HTML and helpful links. Download all our examples to get started.</p>
-
-    <div class="mb-5">
-      <a href="/docs/5.0/examples/" class="btn btn-primary btn-lg px-4">Download examples</a>
-    </div>
-
+  <h4><?= $post['id'] ?> </h4>
+<h1><?= $post['title'] ?> </h1>
+    <p class="fs-5 col-md-8"><?php echo $post['body'] ?></p>
+    
+    <p><?= $post['created_at'] ?></p>
     <hr class="col-3 col-md-2 mb-5">
 
-    <div class="row g-5">
-      <div class="col-md-6">
-        <h2>Starter projects</h2>
-        <p>Ready to beyond the starter template? Check out these open source projects that you can quickly duplicate to a new GitHub repository.</p>
-        <ul class="icon-list">
-          <li><a href="https://github.com/twbs/bootstrap-npm-starter" rel="noopener" target="_blank">Bootstrap npm starter</a></li>
-          <li class="text-muted">Bootstrap Parcel starter (coming soon!)</li>
-        </ul>
-      </div>
-
-     
-    </div>
 </div>
 
 
